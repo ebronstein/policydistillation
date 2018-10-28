8d2e8a8ea764fafad0826f0a988bd5d01612729e
Eli Bronstein <ebronstein@berkeley.edu>
2018-10-28 14:46:22 -0700

    Add dqn_utils.py

---
+++ utils/dqn_utils.py
@@ -0,0 +1,173 @@
+"""This file includes a collection of utility functions that are useful for
+implementing DQN."""
+import gym
+import tensorflow as tf
+import numpy as np
+import random
+
+def huber_loss(x, delta=1.0):
+    # https://en.wikipedia.org/wiki/Huber_loss
+    return tf.where(
+        tf.abs(x) < delta,
+        tf.square(x) * 0.5,
+        delta * (tf.abs(x) - 0.5 * delta)
+    )
+
+def sample_n_unique(sampling_f, n):
+    """Helper function. Given a function `sampling_f` that returns
+    comparable objects, sample n such unique objects.
+    """
+    res = []
+    while len(res) < n:
+        candidate = sampling_f()
+        if candidate not in res:
+            res.append(candidate)
+    return res
+
+class Schedule(object):
+    def value(self, t):
+        """Value of the schedule at time t"""
+        raise NotImplementedError()
+
+class ConstantSchedule(object):
+    def __init__(self, value):
+        """Value remains constant over time.
+        Parameters
+        ----------
+        value: float
+            Constant value of the schedule
+        """
+        self._v = value
+
+    def value(self, t):
+        """See Schedule.value"""
+        return self._v
+
+def linear_interpolation(l, r, alpha):
+    return l + alpha * (r - l)
+
+class PiecewiseSchedule(object):
+    def __init__(self, endpoints, interpolation=linear_interpolation, outside_value=None):
+        """Piecewise schedule.
+        endpoints: [(int, int)]
+            list of pairs `(time, value)` meaning that schedule should output
+            `value` when `t==time`. All the values for time must be sorted in
+            an increasing order. When t is between two times, e.g. `(time_a, value_a)`
+            and `(time_b, value_b)`, such that `time_a <= t < time_b` then value outputs
+            `interpolation(value_a, value_b, alpha)` where alpha is a fraction of
+            time passed between `time_a` and `time_b` for time `t`.
+        interpolation: lambda float, float, float: float
+            a function that takes value to the left and to the right of t according
+            to the `endpoints`. Alpha is the fraction of distance from left endpoint to
+            right endpoint that t has covered. See linear_interpolation for example.
+        outside_value: float
+            if the value is requested outside of all the intervals sepecified in
+            `endpoints` this value is returned. If None then AssertionError is
+            raised when outside value is requested.
+        """
+        idxes = [e[0] for e in endpoints]
+        assert idxes == sorted(idxes)
+        self._interpolation = interpolation
+        self._outside_value = outside_value
+        self._endpoints      = endpoints
+
+    def value(self, t):
+        """See Schedule.value"""
+        for (l_t, l), (r_t, r) in zip(self._endpoints[:-1], self._endpoints[1:]):
+            if l_t <= t and t < r_t:
+                alpha = float(t - l_t) / (r_t - l_t)
+                return self._interpolation(l, r, alpha)
+
+        # t does not belong to any of the pieces, so doom.
+        assert self._outside_value is not None
+        return self._outside_value
+
+class LinearSchedule(object):
+    def __init__(self, schedule_timesteps, final_p, initial_p=1.0):
+        """Linear interpolation between initial_p and final_p over
+        schedule_timesteps. After this many timesteps pass final_p is
+        returned.
+        Parameters
+        ----------
+        schedule_timesteps: int
+            Number of timesteps for which to linearly anneal initial_p
+            to final_p
+        initial_p: float
+            initial output value
+        final_p: float
+            final output value
+        """
+        self.schedule_timesteps = schedule_timesteps
+        self.final_p            = final_p
+        self.initial_p          = initial_p
+
+    def value(self, t):
+        """See Schedule.value"""
+        fraction  = min(float(t) / self.schedule_timesteps, 1.0)
+        return self.initial_p + fraction * (self.final_p - self.initial_p)
+
+def compute_exponential_averages(variables, decay):
+    """Given a list of tensorflow scalar variables
+    create ops corresponding to their exponential
+    averages
+    Parameters
+    ----------
+    variables: [tf.Tensor]
+        List of scalar tensors.
+    Returns
+    -------
+    averages: [tf.Tensor]
+        List of scalar tensors corresponding to averages
+        of al the `variables` (in order)
+    apply_op: tf.runnable
+        Op to be run to update the averages with current value
+        of variables.
+    """
+    averager = tf.train.ExponentialMovingAverage(decay=decay)
+    apply_op = averager.apply(variables)
+    return [averager.average(v) for v in variables], apply_op
+
+def minimize_and_clip(optimizer, objective, var_list, clip_val=10):
+    """Minimized `objective` using `optimizer` w.r.t. variables in
+    `var_list` while ensure the norm of the gradients for each
+    variable is clipped to `clip_val`
+    """
+    gradients = optimizer.compute_gradients(objective, var_list=var_list)
+    for i, (grad, var) in enumerate(gradients):
+        if grad is not None:
+            gradients[i] = (tf.clip_by_norm(grad, clip_val), var)
+    return optimizer.apply_gradients(gradients)
+
+def initialize_interdependent_variables(session, vars_list, feed_dict):
+    """Initialize a list of variables one at a time, which is useful if
+    initialization of some variables depends on initialization of the others.
+    """
+    vars_left = vars_list
+    while len(vars_left) > 0:
+        new_vars_left = []
+        for v in vars_left:
+            try:
+                # If using an older version of TensorFlow, uncomment the 1st line
+                # below and comment out the line after it (the 2nd line).
+                # session.run(tf.initialize_variables([v]), feed_dict)
+                session.run(tf.variables_initializer([v]), feed_dict)
+            except tf.errors.FailedPreconditionError:
+                new_vars_left.append(v)
+        if len(new_vars_left) >= len(vars_left):
+            # This can happend if the variables all depend on each other, or more likely if there's
+            # another variable outside of the list, that still needs to be initialized. This could be
+            # detected here, but life's finite.
+            raise Exception("Cycle in variable dependencies, or external precondition unsatisfied.")
+        else:
+            vars_left = new_vars_left
+
+def get_wrapper_by_name(env, classname):
+    currentenv = env
+    while True:
+        if classname in currentenv.__class__.__name__:
+            return currentenv
+        elif isinstance(env, gym.Wrapper):
+            currentenv = currentenv.env
+        else:
+            raise ValueError("Couldn't find wrapper named %s"%classname)
+
