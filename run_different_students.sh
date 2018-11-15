ddaf176af0a62d68cc93d7366cc68dfecf5fe33a
Eli Bronstein <ebronstein@berkeley.edu>
2018-11-15 14:48:40 -0800

    Update run_different_students.sh

--- run_different_students.sh
+++ run_different_students.sh
@@ -2,17 +2,36 @@ set -eux
 
 TEACHER_CHECKPOINT_DIR="results/Pong-v0_greyscale_mse_2018-11-06-13-43-39/teacher/model.weights/"
 
-# MSE with softmax sharpening
-python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_softmax_0.01 $TEACHER_CHECKPOINT_DIR mse softmax -stqt 0.01
+## Softmax sharpening
 
-# NLL with softmax sharpening
-python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_nll_softmax_0.01 $TEACHER_CHECKPOINT_DIR nll softmax -stqt 0.01
+# MSE of action probabilities with softmax sharpening
+python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob_softmax_0.01 $TEACHER_CHECKPOINT_DIR mse_prob softmax_tau -stqt 0.01
+
+
+## No softmax
 
 # KL without softmax sharpening/softening
 python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_kl $TEACHER_CHECKPOINT_DIR kl none
 
-# KL with softmax softening
-python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_kl_softmax_2 $TEACHER_CHECKPOINT_DIR kl softmax -stqt 2
 
-# MSE with softmax softening
-python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_softmax_2 $TEACHER_CHECKPOINT_DIR mse softmax -stqt 2
+## Softmax softening
+# KL with softmax softening
+python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_kl_softmax_2 $TEACHER_CHECKPOINT_DIR kl softmax_tau -stqt 2
+
+# MSE of action probabilities with softmax softening
+python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob_softmax_2 $TEACHER_CHECKPOINT_DIR mse_prob softmax_tau -stqt 2
+
+
+## DON'T DO THESE
+
+# MSE of Q values with softmax sharpening
+# This will do the MSE between the teacher Q values divided by tau and the student
+# Q values, which is kinda the same as just doing the MSE
+python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_qval_softmax_0.01 $TEACHER_CHECKPOINT_DIR mse_qval softmax_tau -stqt 0.01
+
+# MSE of action probabilities with or without softmax sharpening/softening
+# This will just make the student learn through the softmax function, which isn't
+# necessary unless we want the student's Q values to be of the same scale
+# relative to teach other but of a different absolute scale (since only the relative
+# scale affects the action probabilities due to the softmax).
+python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob $TEACHER_CHECKPOINT_DIR mse_prob
