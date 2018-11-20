3775fd7adf2b21180e24bba13abff201e06adba9
Eli Bronstein <ebronstein@berkeley.edu>
2018-11-19 17:40:45 -0800

    Update run_different_students.sh to run MSE + NLL

--- run_different_students.sh
+++ run_different_students.sh
@@ -5,23 +5,27 @@ TEACHER_CHECKPOINT_DIR="results/Pong-v0_greyscale_mse_2018-11-06-13-43-39/teache
 ## Softmax sharpening
 
 # MSE of action probabilities with softmax sharpening
-# max evaluated reward: 11.32
-python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob_softmax_0.01 $TEACHER_CHECKPOINT_DIR mse_prob softmax_tau -stqt 0.01
+# DONE: max evaluated reward: 11.32
+# python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob_softmax_0.01 $TEACHER_CHECKPOINT_DIR mse_prob softmax_tau -stqt 0.01
+
+# MSE of action probabilities with softmax sharpening + NLL
+python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob_nll_softmax_0.01 $TEACHER_CHECKPOINT_DIR mse_prob_nll softmax_tau -stqt 0.01
 
 
 ## No softmax
 
 # KL without softmax sharpening/softening
-# max evaluated reward: 11.18
-python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_kl $TEACHER_CHECKPOINT_DIR kl none
+# DONE: max evaluated reward: 11.18
+# python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_kl $TEACHER_CHECKPOINT_DIR kl none
 
 
 ## Softmax softening
-# KL with softmax softening
-python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_kl_softmax_2 $TEACHER_CHECKPOINT_DIR kl softmax_tau -stqt 2
+# DONE: KL with softmax softening
+# python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_kl_softmax_2 $TEACHER_CHECKPOINT_DIR kl softmax_tau -stqt 2
 
 # MSE of action probabilities with softmax softening
-python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob_softmax_2 $TEACHER_CHECKPOINT_DIR mse_prob softmax_tau -stqt 2
+# DONE:
+# python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob_softmax_2 $TEACHER_CHECKPOINT_DIR mse_prob softmax_tau -stqt 2
 
 
 ## DON'T DO THESE
@@ -29,11 +33,12 @@ python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob_softmax_2 $TEACHE
 # MSE of Q values with softmax sharpening
 # This will do the MSE between the teacher Q values divided by tau and the student
 # Q values, which is kinda the same as just doing the MSE
-python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_qval_softmax_0.01 $TEACHER_CHECKPOINT_DIR mse_qval softmax_tau -stqt 0.01
+# DONE: 
+# python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_qval_softmax_0.01 $TEACHER_CHECKPOINT_DIR mse_qval softmax_tau -stqt 0.01
 
 # MSE of action probabilities with or without softmax sharpening/softening
 # This will just make the student learn through the softmax function, which isn't
 # necessary unless we want the student's Q values to be of the same scale
 # relative to teach other but of a different absolute scale (since only the relative
 # scale affects the action probabilities due to the softmax).
-python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob $TEACHER_CHECKPOINT_DIR mse_prob
+# python distilledqn_atari.py Pong-v0 Pong-v0_greyscale_mse_prob $TEACHER_CHECKPOINT_DIR mse_prob
