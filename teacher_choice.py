172a88726cff7bc035832f97e745bfaafc7d2791
Eli Bronstein <ebronstein@berkeley.edu>
2018-12-04 12:40:51 -0800

    Initial commit of teacher_choice.py

---
+++ teacher_choice.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class TeacherChoice(object):
+    def choose_teacher(self, t):
+        raise NotImplementedError
+
+class NonContextualBandit(TeacherChoice):
+    def __init__(self, num_teachers):
+        self.num_teachers = num_teachers
+        # number of times each teacher was chosen
+        self.num_times_chosen = np.zeros(num_teachers)
+        # list of rewards received after learning from each teacher
+        self.rewards = [[] for _ in range(num_teachers)]
+        # the teacher that was previously chosen. Init to random
+        self.prev_chosen_teacher = np.random.choice(num_teachers)
+
+class EpsilonGreedyBandit(NonContextualBandit):
+    def __init__(self, num_teachers, eps_schedule):
+        super(NonContextualBandit, self).__init__(num_teachers)
+        self.eps_schedule = eps_schedule
+
+    def update_schedule(self, t):
+        self.eps_schedule.update(t)
+
+    def max_reward_teacher(self):
+        """Return the teacher with the largest associated average reward."""
+        return np.argmax(np.mean(r) for r in self.rewards)
+
+    def choose_teacher(self):
+        """Choose the teacher using epsilon-greedy strategy and update the number
+        of times that teacher was chosen.
+        """
+        if np.random.random() <= self.eps_schedule.epsilon:
+            # choose a random teacher
+            teacher = np.random.choice(self.num_teachers)
+        else:
+            teacher = self.max_reward_teacher()
+        self.num_times_chosen[teacher] += 1
+        self.prev_chosen_teacher = teacher
+        return teacher
+
+    # def store_teacher_rewards(self, reward):
+    #     if hasattr(reward, '__iter__'):
+
+    #     else:
