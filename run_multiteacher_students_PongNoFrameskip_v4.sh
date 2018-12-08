33646ba3127fc075865f945303741253205e797c
Eli Bronstein <ebronstein@berkeley.edu>
2018-12-07 17:23:25 -0800

    Fix run script

--- run_multiteacher_students_PongNoFrameskip_v4.sh
+++ run_multiteacher_students_PongNoFrameskip_v4.sh
@@ -15,4 +15,4 @@ TEACHER_HUBER_NAME="teacher_PongNoFrameskip-v4_greyscale_huber_teacher"
 ### RandomBandit
 
 # Learn from student_Pong-v0_greyscale_mse_prob_softmax_0.01 and student_Pong-v0_greyscale_kl
-python distilledqn_atari.py PongNoFrameskip-v4 PongNoFrameskip_v4_multiteacher_kl_softmax_0.01_randombandit kl softmax_tau random_bandit -tcd $STUDENT_KL_DIR $STUDENT_MSE_PROB_DIR $STUDENT_NLL_DIR $TEACHER_HUBER_DIR  -tcn $STUDENT_KL_name $STUDENT_MSE_PROB_name $STUDENT_NLL_name $TEACHER_HUBER_name -stqt 0.01 -nt 5000000
+python distilledqn_atari.py PongNoFrameskip-v4 PongNoFrameskip_v4_multiteacher_kl_softmax_0.01_randombandit kl softmax_tau random_bandit -tcd $STUDENT_KL_DIR $STUDENT_MSE_PROB_DIR $STUDENT_NLL_DIR $TEACHER_HUBER_DIR  -tcn $STUDENT_KL_NAME $STUDENT_MSE_PROB_NAME $STUDENT_NLL_NAME $TEACHER_HUBER_NAME -stqt 0.01 -nt 5000000
