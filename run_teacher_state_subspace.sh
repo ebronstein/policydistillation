34dbac193b14fac9e47d2abe77b9221c7a5561c0
Eli Bronstein <ebronstein@berkeley.edu>
2018-12-12 10:27:29 -0800

    Run bottom half

--- run_teacher_state_subspace.sh
+++ run_teacher_state_subspace.sh
@@ -1,4 +1,7 @@
 set -eux
 
 # top half
-python natureqn_atari.py PongNoFrameskip-v4 PongNoFrameskip-v4_greyscale_huber_top_half_teacher -nt 10000000 -qns large -ss ball_top_half
+# python natureqn_atari.py PongNoFrameskip-v4 PongNoFrameskip-v4_greyscale_huber_top_half_teacher -nt 10000000 -qns large -ss ball_top_half
+
+# bottom half
+python natureqn_atari.py PongNoFrameskip-v4 PongNoFrameskip-v4_greyscale_huber_bottom_half_teacher -nt 10000000 -qns large -ss ball_bottom_half
